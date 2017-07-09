1ce5967b5bd56e3ab4ed921b6d9404e0c6122bcc
NatalijaGucevska <natalija.gucevska@epfl.ch>
2017-07-09 19:58:58 +0200

    Method signature

---
+++ Language.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Language {
+	ArrayList<String> sortedWords; 
+	
+	public Language(ArrayList<String> sortedWords) {
+		this.sortedWords = sortedWords;
+	}
+	
+	public List<Character> getSortedAlphabet() {
+		List<Character> alphabet = new ArrayList<Character>();
+
+		return alphabet; 
+	}
+}
