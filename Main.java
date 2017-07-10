6b3660f4586ddeac0f73348429c94f3a84f0920c
NatalijaGucevska <natalija.gucevska@epfl.ch>
2017-07-10 23:04:29 +0200

    Add new test

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
+		//==========================================Test from assignment=====================================================
 		List<String> sortedWords = new ArrayList<>(); 
 		sortedWords.add("ART"); 
 		sortedWords.add("RAT"); 
@@ -11,7 +12,19 @@ public class Main {
 		sortedWords.add("CAR"); 
 
 		Language lang = new Language(sortedWords); 
-		System.out.println(lang.getSortedAlphabet().toString());
-	}
+		System.out.println("Expected [A, T, R, C], result: " + lang.getSortedAlphabet().toString());
 
+		
+		//=============================================Test with incorrect order=================================================
+		sortedWords = new ArrayList<>(); 
+		sortedWords.add("ART"); 
+		sortedWords.add("RAT"); 
+		sortedWords.add("CAT"); 
+		sortedWords.add("CAR"); 
+		sortedWords.add("AAR"); 
+	
+	
+		lang = new Language(sortedWords); 
+		System.out.println("Expected [], result: " + lang.getSortedAlphabet().toString());
+	}
 }
