2b3a60f58694a13bb273cadf2b2d374436249375
NatalijaGucevska <natalija.gucevska@epfl.ch>
2017-07-10 20:50:14 +0200

    Build graph from sorted dictionary

--- Language.java
+++ Language.java
@@ -13,4 +13,36 @@ public class Language {
 
 		return alphabet; 
 	}
+	/**
+	 * In order to perform topological sort we need to build a graph from the
+	 * sorted words of the unknown language
+	 */
+	private Map<Character, Set<Character>> buildGraph() {
+		Map<Character, Set<Character>> graph = new HashMap<Character, Set<Character>>();
+
+		for (int i = 0; i < sortedWords.size() - 1; i++) {
+			String currenttWord = sortedWords.get(i);
+			String adjacentWord = sortedWords.get(i + 1);
+
+			boolean wait = true;
+			int count = 0;
+			int limit = Math.min(currenttWord.length(), adjacentWord.length());
+
+			while (wait) {
+				if (currenttWord.charAt(count) != adjacentWord.charAt((count))) {
+					Set<Character> set = graph.get(currenttWord.charAt(count));
+					if (set == null) {
+						set = new HashSet<>();
+						graph.put(currenttWord.charAt(count), set);
+					}
+					set.add(adjacentWord.charAt(count));
+					wait = false;
+				}
+				count++;
+				wait = count < limit;
+			}
+		}
+		return graph;
+	}
+
 }
