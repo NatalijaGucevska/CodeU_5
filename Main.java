0c03300264a98151f4fc74247712a467be1e201e
NatalijaGucevska <natalija.gucevska@epfl.ch>
2017-07-11 01:27:21 +0200

    Fix indentation

--- Main.java
+++ Main.java
@@ -4,62 +4,60 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		//==========================================Test from assignment=====================================================
-		List<String> sortedWords = new ArrayList<>(); 
-		sortedWords.add("ART"); 
-		sortedWords.add("RAT"); 
-		sortedWords.add("CAT"); 
-		sortedWords.add("CAR"); 
+		// ==========================================Test from assignment=====================================================
+		List<String> sortedWords = new ArrayList<>();
+		sortedWords.add("ART");
+		sortedWords.add("RAT");
+		sortedWords.add("CAT");
+		sortedWords.add("CAR");
 
-		Language lang = new Language(sortedWords); 
+		Language lang = new Language(sortedWords);
 		System.out.println("Expected [A, T, R, C], result: " + lang.getSortedAlphabet().toString());
 
-		
-		//=============================================Test with incorrect order=================================================
-		sortedWords = new ArrayList<>(); 
-		sortedWords.add("ART"); 
-		sortedWords.add("RAT"); 
-		sortedWords.add("CAT"); 
-		sortedWords.add("CAR"); 
-		sortedWords.add("AAR"); 
-	
+		// =============================================Test with incorrect order=================================================
+		sortedWords = new ArrayList<>();
+		sortedWords.add("ART");
+		sortedWords.add("RAT");
+		sortedWords.add("CAT");
+		sortedWords.add("CAR");
+		sortedWords.add("AAR");
+
 		System.out.print("Expected -Graph contains cycles-, result: ");
-		lang = new Language(sortedWords); 
+		lang = new Language(sortedWords);
 		try {
 			lang.getSortedAlphabet();
-		} catch(IllegalArgumentException e ) {
+		} catch (IllegalArgumentException e) {
 			System.out.println("-" + e.getMessage() + "-");
 		}
-		
-		//==========================================Test with only one word=====================================================
-				sortedWords = new ArrayList<>(); 
-				sortedWords.add("ART"); 
 
-				lang = new Language(sortedWords); 
-				System.out.println("Expected [A, R, T], result: " + lang.getSortedAlphabet().toString());
-				
-		//==========================================Test with two empty words=====================================================
-				sortedWords = new ArrayList<>(); 
-				sortedWords.add(""); 
-				sortedWords.add(""); 
+		// ==========================================Test with only one word=====================================================
+		sortedWords = new ArrayList<>();
+		sortedWords.add("ART");
 
-				lang = new Language(sortedWords); 
-				System.out.println("Expected [], result: " + lang.getSortedAlphabet().toString());
-		//==========================================Test with two words from which one is empty=====================================================
-				sortedWords = new ArrayList<>(); 
-				sortedWords.add(""); 
-				sortedWords.add("ART"); 
+		lang = new Language(sortedWords);
+		System.out.println("Expected [A, R, T], result: " + lang.getSortedAlphabet().toString());
 
-				lang = new Language(sortedWords); 
-				System.out.println("Expected [A, R, T], result: " + lang.getSortedAlphabet().toString());
-	   //==========================================Test with words of different legth=====================================================
-				sortedWords = new ArrayList<>(); 
-				sortedWords.add("A"); 
-				sortedWords.add("ART"); 
-				sortedWords.add("CRTa"); 
-				
+		// ==========================================Test with two empty words=====================================================
+		sortedWords = new ArrayList<>();
+		sortedWords.add("");
+		sortedWords.add("");
 
-				lang = new Language(sortedWords); 
-				System.out.println("Expected [A, a, R, T, C], result: " + lang.getSortedAlphabet().toString());
+		lang = new Language(sortedWords);
+		System.out.println("Expected [], result: " + lang.getSortedAlphabet().toString());
+		// ==========================================Test with two words from which one is empty=====================================================
+		sortedWords = new ArrayList<>();
+		sortedWords.add("");
+		sortedWords.add("ART");
+
+		lang = new Language(sortedWords);
+		System.out.println("Expected [A, R, T], result: " + lang.getSortedAlphabet().toString());
+		// ==========================================Test with words of different legth=====================================================
+		sortedWords = new ArrayList<>();
+		sortedWords.add("A");
+		sortedWords.add("ART");
+		sortedWords.add("CRTa");
+
+		lang = new Language(sortedWords);
+		System.out.println("Expected [A, a, R, T, C], result: " + lang.getSortedAlphabet().toString());
 	}
 }
